aee782fc071afb7d48536c7c09d40b6feb18a45f
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-02-17 13:48:45 +0300

    Görev 1 için paketler oluşturuldu

---
+++ src/main/java/com/workintech/s18d2/entity/Fruit.java
@@ -0,0 +1,11 @@
+package com.workintech.s18d2.entity;
+
+public class Fruit {
+
+
+    private Long id;
+    private String name;
+    private double price;
+    private FruitType fruitType;
+
+}
---
+++ src/main/java/com/workintech/s18d2/entity/Vegetable.java
@@ -0,0 +1,10 @@
+package com.workintech.s18d2.entity;
+
+public class Vegetable {
+
+    private  Long id;
+    private String name;
+    private  double price;
+    private boolean isGrownOnTree;
+
+}
